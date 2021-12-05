e3dad099ef1c91a2b50b56e2f76483867f37492d
Admin <baha72157@gmail.com>
2021-12-05 18:18:42 +0300

    aa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,24 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        double[] numbers = {13.2, 7.4, -7.1, 54.5, -8.2, -5.23, 3.1, 5.5, 84.4, 94.3, -9.9, 45.4, 3.3, 74.3, 3.9, 56.7};
-        double sum = 0;
-        int negativeNumbers = 0;
-        boolean negative = false;
-        for (double d : numbers) {
-            if (d < 0) {
-                negative = true;
-            } else {
-                if (negative) {
-                    negativeNumbers++;
-                    sum += d;
+
+        int[]distribution=new int[]{-56, 4, -22, 90, 31,56};
+
+        for (int i = 0; i < distribution.length - 1; i++) {
+            for (int j = distribution.length - 1; j > i; j--) {
+                if (distribution[j - 1] > distribution[j]) {
+                    int tmp = distribution[j - 1];
+                    distribution[j - 1] = distribution[j];
+                    distribution[j] = tmp;
                 }
             }
-
-
         }
-        System.out.println("Average " + sum / negativeNumbers);
-
+        System.out.println(Arrays.toString(distribution));
 
 
     }
-
-
 }
-
